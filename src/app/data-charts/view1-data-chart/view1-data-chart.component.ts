a5e4d8101d609d498f98593a170fdbd44e48fe99
Zeus_The_Cat <d.s.pettry@gmail.com>
2019-05-24 11:48:59 -0400

    Commented out updateBinding

--- src/app/data-charts/view1-data-chart/view1-data-chart.component.ts
+++ src/app/data-charts/view1-data-chart/view1-data-chart.component.ts
@@ -3,7 +3,7 @@ import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 import { ChartsModule } from 'ng2-charts';
 import { DataTableService } from '../../Service/data-table.service';
-import { updateBinding } from '@angular/core/src/render3/instructions';
+//import { updateBinding } from '@angular/core/src/render3/instructions';
 import { Observable } from 'rxjs';
 import {view1 } from 'src/model/view1.model';
 
@@ -18,7 +18,7 @@ export class View1DataChartComponent implements OnInit {
   private updatedData: view1[]=[];
   public barChartOptions: ChartOptions = {
     responsive: true,
-    
+
     // We use these empty structures as placeholders for dynamic theming.
     scales: { xAxes: [{}], yAxes: [{}] },
     plugins: {
@@ -33,7 +33,7 @@ export class View1DataChartComponent implements OnInit {
   public barChartLegend = true;
   public barChartPlugins = [pluginDataLabels];
   // public barChartData: ChartDataSets[];
-  
+
   public barChartData: ChartDataSets[] = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
@@ -57,23 +57,23 @@ export class View1DataChartComponent implements OnInit {
     // ];
     // console.log("data:id ",this.data._id);
     // console.log("data:id ",this.data.percOfLand);
-    //const dataObservable = 
+    //const dataObservable =
     this.dataTable.getData()
     .subscribe((data: view1[])=>{
         this.updatedData=data;
             console.log("data: ",this.updatedData);
-           
+
     });
     // barChartLabels:this.updatedData._id;
     // barChartData : [
     //   { data: data.percOfAssessedVal, label: 'Series A' },
     //   { data: data.percOfLand, label: 'Series B' }
     // ];
-    
+
   }
   ngAfterViewChecked(){
     this.update();
-    
+
   }
   // events
   public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
