47abd216ef76dc054ab44abe7d67ffd8014008bf
Peter Parianos <peter.parianos@yahoo.com>
2019-06-18 12:44:23 -0400

    added get parcels router

--- backend/router/geometry.js
+++ backend/router/geometry.js
@@ -7,28 +7,18 @@ var db = require("../model/db.js");
 //--------------------------------------------------Show  geometric features  for selected neighbourhood--------------------------------------
 router.get("/showgeometry/:city/:hood?",(req,res,next)=>{
     this.city=req.params.city;
-    this.hood = req.params
-    console.log("typeof: " + typeof req.params.city);
+    this.hood = req.params;
     var query = {};
-    this.city= upperCase(this.city); // all names are capital in the database
+    this.city= upperCase(this.city); // all city names are capital in the database
                                      // only the first letter of the neighbourhood is capital
     query["properties.PAR_CITY"] = this.city;
 
     var newJson = {};
-    
-    console.log(req.body, this.city);
-
     //check to see if hood is undefined, else show all
     if(req.params.hood !== undefined){
       query["properties.SPA_NAME"] = req.params.hood;
     }
 
-    console.log("This hood: " + this.hood);
-
-    //db.distinct("geometry",{"properties.SPA_NAME":"Stockyards"})
-    //db.findOne({"properties.SPA_NAME":this.hood,"properties.PAR_CITY":this.city},{_id:1,"properties.PARCELPIN":1,"properties.PAR_CITY":1,"properties.SPA_NAME":1,"geometry.type":1,"geometry.coordinates":1},(err,result)=>{
-    //.exec(function(err, result) {
-    //
       db.find(query,
 			        {_id:0, type:1,"properties.PARCELPIN":1,"properties.PAR_CITY":1, "properties.SiteCat1":1,
               "properties.SPA_NAME":1,"geometry.type":1,"geometry.coordinates":1},
@@ -52,5 +42,41 @@ router.get("/showgeometry/:city/:hood?",(req,res,next)=>{
     });
 
   });
+
+module.exports=router;
+
+router.get("/getparcels/:geoObject?" , (req, res, next ) => {
+  // this.obj should be in the form of a geojson object { "type" : "Polygon",coordinates" : [[...]]}
+    if(req.params.geoObject === undefined)  res.status(500).send("Error: Please Specify geoObject");
+    try { // convert string form parameter to JSON. will fail if not encapsulated in braces
+    this.obj = JSON.parse(req.params.geoObject);
+    } catch(e) {
+      res.send("error: " + e);
+    }
+    //res.send(typeof this.obj + " value: " + JSON.stringify(this.obj.types)); 
+
+    db.find( {
+      geometry: {
+         $geoIntersects : {
+            $geometry: this.obj
+          }
+      }
+   },
+   {
+           "properties.PARCELPIN" : 1,
+           "properties.SPA_NAME" : 1
+   }, (err , result) => {
+     if (err) {
+       res.send(err);
+       console.log("error occurred");
+     }else{
+       console.log(result);
+       res.json(result);
+     }
+   });
+
+
+
+})
 
 module.exports=router;
